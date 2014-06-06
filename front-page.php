9ac9136a457ed3094ef5622a0650fd33febfa547
Meagan O'Hara <Gnomecake@Meagans-MacBook-Pro.local>
2014-06-06 19:11:24 -0400

    put content into three spaced out containers

--- front-page.php
+++ front-page.php
@@ -6,62 +6,78 @@ Template Name: Front Page
 
 get_header(); ?>
 
-<div class="innerWrapper">
-	<div class="section">
-		<div class="full">
+	<main role="main">
+		<!-- section -->
+		<section class="innerWrapper clear">
+			<!-- section -->
+			<section class="featured clear">
 
-			<?php
+				<h2>Featured Work</h2>
 
-			$portfolioQuery = new WP_Query( 
-				array( 
-					'posts_per_page' =>3,
-					'post_type' => 'portfolio', 
-					'orderby' => 'menu_order',
-					'order' => 'ASC',
-					'featured' => 'Featured'/*,
-					'post__not_in' => array( 6 ) // if you wanted to exclude a page with the ID of 42*/
-					) 
-			); ?>
+				<?php
 
-			<?php if ( $portfolioQuery->have_posts() ) : ?>
+				$portfolioQuery = new WP_Query( 
+					array( 
+						'posts_per_page' =>3,
+						'post_type' => 'portfolio', 
+						'orderby' => 'menu_order',
+						'order' => 'ASC',
+						'featured' => 'Featured'/*,
+						'post__not_in' => array( 6 ) // if you wanted to exclude a page with the ID of 42*/
+						) 
+				); ?>
 
-				<?php while ($portfolioQuery->have_posts()) : $portfolioQuery->the_post(); ?>
+				<?php if ( $portfolioQuery->have_posts() ) : ?>
 
-					<section id="<?php echo $post->post_name; ?>">
-						<a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3>
-						<?php 
-						if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-						  the_post_thumbnail();
-						} 
-						?>
-						<?php the_content(); ?></a>
-					</section>
-				<?php endwhile; ?>
-				
-				<?php wp_reset_postdata(); ?>
-				
-			<?php else:  ?>
-				[stuff that happens if there aren't any posts]
-			<?php endif; ?>
+					<?php while ($portfolioQuery->have_posts()) : $portfolioQuery->the_post(); ?>
 
-			</div>
-		</div> <!-- .section -->
-	</div> <!-- .innerWrapper -->
+						<section id="<?php echo $post->post_name; ?>">
 
-	<div class="services">
+							<a href="<?php the_permalink(); ?>">
+								<figcaption>
+									<h3><?php the_title(); ?></h3>
+									<h4><?php the_field('technologies'); ?></h4>
+								</figcaption>
+								<figure>
+									<?php 
+										if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+										  the_post_thumbnail();
+										} 
+									?>
+								</figure>
+							</a>
+						</section>
+					<?php endwhile; ?>
+					
+					<?php wp_reset_postdata(); ?>
+					
+				<?php else:  ?>
+					[stuff that happens if there aren't any posts]
+				<?php endif; ?>
 
-		<h2>Services</h2>
-		 
-	    <ul>
-	 
-	    <?php while( has_sub_field('services') ): ?>
-	    	<li>
-	    		<p><?php the_sub_field('services_desc'); ?></p>
+			</section> <!-- /section -->
 
-	    	</li>
-	    <?php endwhile; ?>
-	 
-	    </ul>
-	</div>
+			<section class="services">
+
+				<h2>Services</h2>
+				 
+			    <?php while( has_sub_field('services') ): ?>
+
+			    		<section id="<?php echo the_sub_field('services_title'); ?>">
+
+						<h3><?php the_sub_field('services_title'); ?></h3>
+			    		<p><?php the_sub_field('services_desc'); ?></p>
+
+			    		</section>
+			
+			    <?php endwhile; ?>
+
+			    		<?php wp_reset_postdata(); ?>
+
+			</section>
+			<!-- /section -->
+		</section>
+		<!-- /section -->
+	</main>
 
 <?php get_footer(); ?>
