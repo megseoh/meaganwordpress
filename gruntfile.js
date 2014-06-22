b4b7b303048a5cadcd6e897226c64d1efb46ae2c
megseoh <ohara.meagan@gmail.com>
2014-06-22 14:08:35 -0400

    added gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {                                             // Task
+            dist: {                                         // Target
+                options: {                                  // Target options
+                    style: 'expanded'
+                },
+                files: {                                    // Dictionary of files
+                    'css/style.css': 'sass/style.scss'      // 'destination': 'source'
+                }
+            }
+        },
+        watch: {
+        	scss: {
+        		files: ['**/*.scss'],
+        		tasks: ['sass']
+        	},
+        }
+    });
+
+    // Load the Sass tasks, watch tasks
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['watch']);
+
+};
